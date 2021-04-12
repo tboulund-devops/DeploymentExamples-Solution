e2ca62e3ad6233af3e5f953b2409e7ba857d1566
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2021-04-12 13:34:07 +0200

    Printing output

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cp /usr/share/nginx/html/js/configuration/config.$EnvironmentName.js /usr/share/nginx/html/js/configuration/config.js 2> /dev/null || :
+cp /usr/share/nginx/html/js/configuration/config.$EnvironmentName.js /usr/share/nginx/html/js/configuration/config.js # 2> /dev/null || :
 nginx -g 'daemon off;'
